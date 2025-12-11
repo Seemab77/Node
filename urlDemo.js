0a6a7a44768725f8a282db3c96123337f53ea589
Seemab Ramzan <seemab.ramzan@gmail.com>
2025-12-11 14:01:46 +0500

    Initial commit after reinitializing

---
+++ urlDemo.js
@@ -0,0 +1,24 @@
+import url from 'url';
+
+const urlString = 'https://WWW.google.com/search?q=hello+world';
+
+//URL object
+const urlObj = new URL(urlString);
+console.log(urlObj);
+
+// format()
+console.log(url.format(urlObj));
+
+// import.meta.url - file URL
+console.log(import.meta.url);
+
+// fileURLToPath()
+console.log(url.fileURLToPath(import.meta.url));
+
+console.log(urlObj.search);
+
+const params = new URLSearchParams(urlObj.search);
+console.log(params.get('q'));
+params.append('limit', '5');
+params.delete('limit');
+console.log(params);
